d06e4fa31040720c81a1d37976575936d05c0b55
FolcoISEP <folco.coulau@gmail.com>
2017-10-26 11:11:28 +0200

    rename columns and clean columns

--- ScriptMilitaryAI.R
+++ ScriptMilitaryAI.R
@@ -5,14 +5,21 @@ output$`Message type`[output$V1=="PositionReportClassAScheduled{messageType=Posi
 output$`Message type`[output$V1=="PositionReportClassAResponseToInterrogation{messageType=PositionReportClassAResponseToInterrogation}"]=2
 output$`Message type`[output$V1=="StandardClassBCSPositionReport{messageType=StandardClassBCSPositionReport"]=3
 output$`Message type`[output$V1=="ShipAndVoyageData{messageType=ShipAndVoyageRelatedData"]=4
-colnames(output) <- c("MessageType", "RateOfTurn", "SpeedOverGround", "PositionAccuracy", "Latitude", "Longitude", "CourseOverGround", "TrueHeading", "Timestamp", "specialManeuverIndicator", "raimFlag", "SyncState", "SlotTime-out", "Undefined", "AB", "Data", "Data1", "Metadata", "Received", "repeatIndicator", "sourceMmsi")
 output$V2=gsub("^.*?=","", output$V2)
 output$V3=gsub("^.*?=","", output$V3)
 output$V4=gsub("^.*?=","", output$V4)
-output$V2=gsub("^.*?=","", output$V2)
-output$V2=gsub("^.*?=","", output$V2)
-output$V2=gsub("^.*?=","", output$V2)
-output$V2=gsub("^.*?=","", output$V2)
-output$V2=gsub("^.*?=","", output$V2)
-#output <- output[,c(22,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21)]
+output$V5=gsub("^.*?=","", output$V5)
+output$V6=gsub("^.*?=","", output$V6)
+output$V7=gsub("^.*?=","", output$V7)
+output$V8=gsub("^.*?=","", output$V8)
+output$V9=gsub("^.*?=","", output$V9)
+output$V10=gsub("^.*?=","", output$V10)
+output$V11=gsub("^.*?=","", output$V11)
+output$V12=gsub("^.*?=","", output$V12)
+output$V20=gsub("^.*?=","", output$V20)
+output$V21=gsub("^.*?=","", output$V21)
+output$V22=gsub("^.*?=","", output$V22)
+output$V23=gsub("^.*?=","", output$V23)
+colnames(output) <- c("MessageType", "NavigationStatus", "RateOfTurn", "SpeedOverGround", "PositionAccuracy", "Latitude", "Longitude", "CourseOverGround", "TrueHeading", "Timestamp", "specialManeuverIndicator", "raimFlag", "AISMessage", "SyncState", "SlotTime-out", "Undefined", "AB", "Data", "Data1", "Metadata", "Received", "repeatIndicator", "sourceMmsi")
+output <- output[,c(24,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23)]
 View(output)
